510e4fb4e91dc3cee8e5b1efd22ff5348c7a099e
pedroafonso18 <pedroafonsoprogramador@gmail.com>
2025-04-21 10:17:03 -0300

    feat: Gotten the current directory

--- gui/ui.c
+++ gui/ui.c
@@ -4,14 +4,14 @@ int create_window(void) {
     const int monitor = GetCurrentMonitor();
     const int screen_width = GetMonitorWidth(monitor);
     const int screen_height = GetMonitorHeight(monitor);
-
+    const char* working_dir = GetWorkingDirectory();
     InitWindow(screen_width, screen_height, "Assembly Visualizer");
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(GRAY);
-        DrawText("Hello, world!", 0, 0, 11, WHITE);
+        DrawText(working_dir, screen_width/2, screen_height/2, 11, WHITE);
         EndDrawing();
     }
 
